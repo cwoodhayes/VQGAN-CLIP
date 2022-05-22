4a5b0180b0dfe8a177da683a58dacfdb280eb335
Conor Hayes <conorhay@usc.edu>
2022-05-22 01:12:06 -0400

    now vc'ing the dream input scripts! adding one for leaving LA (first shot)

---
+++ dream-input-scripts/dream-2.sh
@@ -0,0 +1,12 @@
+#-conf checkpoints/faceshq.yaml -ckpt checkpoints/faceshq.ckpt  -i 140 -se 2 -vl 5 \
+#  -p "I'm making out with a faceless man | in the style of van gogh "
+#
+#-i 140 -se 2 -vl 5 \
+#  -p "a crowd of people | desire |  in the style of van gogh"
+
+#-i 70 -se 2 -vl 2 \
+#-p "one person encircled by darkness | a dark room"
+
+#-i 140 -se 2 -vl 5 -p "I cry alone"
+
+#-i 140 -se 2 -vl 5 -p "a white space:.3 | waking up:.7" -ip "dream-outputs/ceiling_fan_wake_up.jpg"
---
+++ dream-input-scripts/dream-leaving-la.sh
@@ -0,0 +1,14 @@
+-conf checkpoints/faceshq.yaml -ckpt checkpoints/faceshq.ckpt  -i 100 -se 2 -vl 5 \
+  -p "I'm leaving LA"
+
+-i 70 -se 2 -vl 2 \
+  -p "Stick my head out of the window and drive away"
+
+-i 70 -se 2 -vl 2 \
+-p "Draggin' memories behind like honeymoon cans on the ground"
+
+-i 70 -se 2 -vl 2 -p "And if God knows I'll be back, he also knows"
+
+-i 70 -se 2 -vl 2 -p "I'll be glad to be gone"
+
+
